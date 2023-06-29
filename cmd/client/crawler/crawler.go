febe3c45fcabed4c58a44148d49cdcbf5bdb6429
Lory-Stan TANASI <stantanasi@gmail.com>
2023-06-29 17:39:49 +0200

    feat(crawler): implement sending/receiving update file

--- cmd/client/crawler/crawler.go
+++ cmd/client/crawler/crawler.go
@@ -106,3 +106,33 @@ func createFile(conn net.Conn) {
 
 	fmt.Printf("%+v\n", response)
 }
+
+func updateFile(conn net.Conn) {
+	request := protocols.UpdateFileRequest{
+		Command: "update-file",
+	}
+	requestJSON, err := json.Marshal(request)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Fprintf(conn, string(requestJSON))
+
+	// Réception de la réponse
+	buffer := make([]byte, 4096)
+	n, err := conn.Read(buffer)
+	if err != nil {
+		fmt.Println("Error reading response:", err)
+		return
+	}
+
+	var response protocols.UpdateFileResponse
+	err = json.Unmarshal(buffer[:n], &response)
+	if err != nil {
+		fmt.Println("Error parsing response:", err)
+		return
+	}
+
+	fmt.Printf("%+v\n", response)
+}
