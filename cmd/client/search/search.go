7f9bb8dcf7123588da57ea28ba8311bfb57ff024
Amine <amine.abbes@estiam.com>
2023-06-29 17:23:19 +0200

    implement revice response

--- cmd/client/search/search.go
+++ cmd/client/search/search.go
@@ -18,7 +18,6 @@ func main() {
 	http.ListenAndServe(":8080", r)
 }
 
-
 func connectToServer() net.Conn {
 	// configuration du serveur //
 	conn, err := net.Dial("tcp", "localhost:1234")
@@ -44,6 +43,23 @@ func getSites(w http.ResponseWriter, r *http.Request) {
 	}
 
 	fmt.Fprintf(conn, string(requestJSON))
+
+	// Réception de la réponse
+	buffer := make([]byte, 4096)
+	n, err := conn.Read(buffer)
+	if err != nil {
+		fmt.Println("Error reading response:", err)
+		return
+	}
+
+	var response protocols.GetSiteResponse
+	err = json.Unmarshal(buffer[:n], &response)
+	if err != nil {
+		fmt.Println("Error parsing response:", err)
+		return
+	}
+
+	fmt.Printf("%+v\n", response)
 }
 
 func postSites(w http.ResponseWriter, r *http.Request) {
@@ -60,6 +76,23 @@ func postSites(w http.ResponseWriter, r *http.Request) {
 	}
 
 	fmt.Fprintf(conn, string(requestJSON))
+
+	// Réception de la réponse
+	buffer := make([]byte, 4096)
+	n, err := conn.Read(buffer)
+	if err != nil {
+		fmt.Println("Error reading response:", err)
+		return
+	}
+
+	var response protocols.CreateSiteResponse
+	err = json.Unmarshal(buffer[:n], &response)
+	if err != nil {
+		fmt.Println("Error parsing response:", err)
+		return
+	}
+
+	fmt.Printf("%+v\n", response)
 }
 
 func getFiles(w http.ResponseWriter, r *http.Request) {
@@ -76,4 +109,21 @@ func getFiles(w http.ResponseWriter, r *http.Request) {
 	}
 
 	fmt.Fprintf(conn, string(requestJSON))
+
+	// Réception de la réponse
+	buffer := make([]byte, 4096)
+	n, err := conn.Read(buffer)
+	if err != nil {
+		fmt.Println("Error reading response:", err)
+		return
+	}
+
+	var response protocols.GetFileResponse
+	err = json.Unmarshal(buffer[:n], &response)
+	if err != nil {
+		fmt.Println("Error parsing response:", err)
+		return
+	}
+
+	fmt.Printf("%+v\n", response)
 }
