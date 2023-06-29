66d7becce1b3e39c2582251f4fcb1be0889fc6c9
Lory-Stan TANASI <stantanasi@gmail.com>
2023-06-29 17:32:23 +0200

    feat(crawler): implement sending/receiving get site

--- cmd/client/crawler/crawler.go
+++ cmd/client/crawler/crawler.go
@@ -1,18 +1,48 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"github.com/stantanasi/crawler-go/pkg/protocols"
 	"net"
 )
 
 func main() {
-	// configuration du serveur //
+	// Configure TCP server
 	conn, err := net.Dial("tcp", "localhost:1234")
 	if err != nil {
 		fmt.Println("Error starting server:", err)
 		return
 	}
 	defer conn.Close()
-	// configuration du serveur //
+}
 
+func getSite(conn net.Conn) {
+	request := protocols.GetSiteRequest{
+		Command: "get-site",
+	}
+	requestJSON, err := json.Marshal(request)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Fprintf(conn, string(requestJSON))
+
+	// Réception de la réponse
+	buffer := make([]byte, 4096)
+	n, err := conn.Read(buffer)
+	if err != nil {
+		fmt.Println("Error reading response:", err)
+		return
+	}
+
+	var response protocols.GetSiteResponse
+	err = json.Unmarshal(buffer[:n], &response)
+	if err != nil {
+		fmt.Println("Error parsing response:", err)
+		return
+	}
+
+	fmt.Printf("%+v\n", response)
 }
