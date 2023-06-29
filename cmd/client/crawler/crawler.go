8499b778bd6c7559c9a9d1d85cceaec3db72f12c
Amine <amine.abbes@estiam.com>
2023-06-29 15:07:32 +0200

    connexion au serveur commit

--- cmd/client/crawler/crawler.go
+++ cmd/client/crawler/crawler.go
@@ -1,4 +1,24 @@
 package main
 
+import (
+	"fmt"
+	"net"
+)
+
 func main() {
+
+	// configuration du serveur //
+	conn, err := net.Dial("tcp", "localhost:8080")
+
+	if err != nil {
+
+		fmt.Println("Error starting server:", err)
+
+		return
+
+	}
+
+	defer conn.Close()
+	// configuration du serveur //
+
 }
