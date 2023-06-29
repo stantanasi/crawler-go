99a5905601e1f4a3a1c0556f13534c017b54716a
Amine <amine.abbes@estiam.com>
2023-06-29 15:04:08 +0200

    index creation commit

--- cmd/server/index.go
+++ cmd/server/index.go
@@ -1,4 +1,17 @@
 package main
 
+import (
+	"fmt"
+	"net"
+)
+
 func main() {
+	// configuration du serveur //
+	_, err := net.Listen("tcp", "0.0.0.0:8080")
+	if err != nil {
+		fmt.Println("Error starting server:", err)
+		return
+	}
+	fmt.Println("Server started!")
+	// configuration du serveur //
 }
