90b5552889d8e50e4ffba9fd0d856f70a75abc27
Lory-Stan TANASI <stantanasi@gmail.com>
2023-06-29 15:17:00 +0200

    refactor(search): reformat code

--- cmd/client/search/search.go
+++ cmd/client/search/search.go
@@ -6,19 +6,12 @@ import (
 )
 
 func main() {
-
 	// configuration du serveur //
 	conn, err := net.Dial("tcp", "localhost:8080")
-
 	if err != nil {
-
 		fmt.Println("Error starting server:", err)
-
 		return
-
 	}
-
 	defer conn.Close()
 	// configuration du serveur //
-
 }
