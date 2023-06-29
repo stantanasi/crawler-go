0b8a12ac04772299a5a881cb5f8bbbdf630cdee0
Lory-Stan TANASI <stantanasi@gmail.com>
2023-06-29 16:29:22 +0200

    feat: create protocols types

---
+++ pkg/protocols/protocols.go
@@ -0,0 +1,54 @@
+package protocols
+
+type CreateSiteRequest struct {
+	Command string `json:"command"`
+}
+
+type CreateSiteReponse struct {
+	Command string `json:"command"`
+}
+
+
+type GetSiteRequest struct {
+	Command string `json:"command"`
+}
+
+type GetSiteResponse struct {
+	Command string `json:"command"`
+}
+
+
+type UpdateSiteRequest struct {
+	Command string `json:"command"`
+}
+
+type UpdateSiteReponse struct {
+	Command string `json:"command"`
+}
+
+
+type CreateFileRequest struct {
+	Command string `json:"command"`
+}
+
+type CreateFileResponse struct {
+	Command string `json:"command"`
+}
+
+
+type GetFileRequest struct {
+	Command string `json:"command"`
+}
+
+type GetFileResponse struct {
+	Command string `json:"command"`
+}
+
+
+type UpdateFileRequest struct {
+	Command string `json:"command"`
+}
+
+type UpdateFileResponse struct {
+	Command string `json:"command"`
+}
